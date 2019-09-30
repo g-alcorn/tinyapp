ced9eeb991382d174298410ee3f4ffcd8938d357
g-alcorn <griffin.alcorn@gmail.com>
2019-09-30 18:03:03 +0000

    now gets and renders short url

--- express_server.js
+++ express_server.js
@@ -17,8 +17,9 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars)
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: "long egg"};
+  res.render("urls_show", templateVars);
 });
 
 app.get("/hello", (req, res) => {
