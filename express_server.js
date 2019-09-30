08842e34645a60df3624fe318c79b08cae661db5
g-alcorn <griffin.alcorn@gmail.com>
2019-09-30 18:50:46 +0000

    updated to generate random string for short url

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -17,14 +19,31 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars)
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  let newURL = generateRandomString();
+  res.send(newURL);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: "long egg"};
   res.render("urls_show", templateVars);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+function generateRandomString() {
+  const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+  let length = characters.length;
+  let result = "";
+  for (let i = 0; i < 5; i++){
+    result += characters.charAt(Math.floor(Math.random() * length));
+  }
+
+  return result;
+}
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
