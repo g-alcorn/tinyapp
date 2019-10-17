ea1a22db6c28b4cf23308db075f5d295ece9fd25
g-alcorn <griffin.alcorn@gmail.com>
2019-10-17 12:58:13 +0000

    helper functions rearranged, login logic fixed, and console messages removed. alert messages non functional

--- express_server.js
+++ express_server.js
@@ -29,6 +29,17 @@ const users = {
   }
 };
 
+//GENERATE SHORT URL STRING
+function generateRandomString() {
+  const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+  let length = characters.length;
+  let result = "";
+  for (let i = 0; i < 5; i++){
+    result += characters.charAt(Math.floor(Math.random() * length));
+  }
+
+  return result;
+};
 //SHOW LIST OF URLs via redirect
 app.get("/", (req, res) => {
   res.redirect("/urls");
@@ -57,10 +68,10 @@ app.get("/urls", (req, res) => {
 
     res.render("urls_index", templateVars);
   } else {
-    console.log("not logged in - redirecting from /urls");
+    let error = "Please login!";
     userId = null;
     let templateVars = {
-      userId
+      error
     };
     res.render("login", templateVars);
   }
@@ -73,7 +84,7 @@ app.get("/urls/new", (req, res) => {
 
   if (!req.session.email) {
     templateVars.userId = null;
-    console.log("not logged in - redirecting");
+    templateVars.error = "Please login!";
     res.render("login", templateVars);
   } else {  
     templateVars.loggedIn = true;
@@ -108,8 +119,7 @@ app.get("/urls/:shortURL", (req, res) => {
 
 //REDIRECT TO LONG URL
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = "http\://" + urlDatabase[req.params.shortURL].longUrl;
-  console.log(longURL);
+  const longURL = "https\://" + urlDatabase[req.params.shortURL].longUrl;
   res.redirect(longURL);
 });
 
@@ -134,9 +144,16 @@ app.get("/login", (req, res) => {
 //REGISTER BUTTON CLICK
 app.post("/register", (req, res) => {
   const { email } = req.body;
-  const { password } = bcrypt.hashSync(req.body.password, 10);
-  let userId = generateRandomString();
+  const input = req.body.password;
+  if (email.length === 0 || input.length === 0) {
+    const templateVars = {
+      error: "All fields required!"
+    };
+    res.redirect("/register", templateVars);
+  }
 
+  const password = bcrypt.hashSync(input, 10);
+  let userId = generateRandomString();
   for (clientId in users) {
     if (email === users[clientId].email) {
       res.redirect("/urls");
@@ -159,7 +176,10 @@ app.post("/register", (req, res) => {
 //GENERATE NEW SHORT URL
 app.post("/urls", (req, res) => {
   if (!req.session.userId) {
-    res.redirect("/urls/");
+    const templateVars = {
+      error: "Please login!"
+    };
+    res.redirect("/urls/", templateVars);
   }
   let newURL = generateRandomString();
   urlDatabase[newURL] = { longUrl: req.body.longURL, userId: req.session.userId };
@@ -175,15 +195,16 @@ app.post("/urls/:shortURL", (req, res) => {
 //DELETE BUTTON CLICK
 app.post("/urls/:shortURL/delete", (req, res) => {
   const toDelete = req.params.shortURL;
+  const templateVars = {};
 
   if (!req.session) {
-    console.log("not logged in - cannot delete");
-    res.redirect("login");
+    templateVars.error = "not logged in - cannot delete";
+    res.redirect("login", templateVars);
   } else if (req.session["userId"] !== urlDatabase[toDelete].userId) {
-    console.log("cannot delete links created by other users!");
-    res.redirect("/urls");
+    templateVars.error = "cannot delete links created by other users!";
+    res.redirect("/urls", templateVars);
   } else {
-    console.log("deleting " + toDelete);
+    //console.log("deleting " + toDelete);
     delete urlDatabase[toDelete];
     res.redirect("/urls");
   }
@@ -192,17 +213,16 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 //UPDATE URL ALREADY IN DATABASE
 app.post("/urls/:shortURL/update", (req, res) => {
   const shortUrl = req.params.shortURL;
-  
+  const templateVars = {};
+
   if (!req.session) {
-    console.log("not logged in - cannot update");
-    res.redirect("login");
+    templateVars.error = "not logged in - cannot update";
+    res.redirect("login", templateVars);
   } else if (req.session.userId !== urlDatabase[shortUrl].userId) {
-    console.log(req.session.userId + " " + shortUrl + " " + urlDatabase[shortUrl].userId);
-    console.log("cannot update links created by other users!");
-    res.redirect("/urls");
+    templateVars.error = "cannot update links created by other users!";
+    res.redirect("/urls", templateVars);
   } else {
     urlDatabase[shortUrl].longUrl = req.body.longURL;
-    console.log("updated URL successfully");
     res.redirect("/urls");    
   }
 });
@@ -210,25 +230,33 @@ app.post("/urls/:shortURL/update", (req, res) => {
 //LOGIN BUTTON PRESS
 app.post("/login", (req, res) => {
   const {email, password } = req.body;
+  const templateVars = {};
   let userId = "";
+  let status = undefined;
   //for loop continues through all users until email AND password match
-  for (client in users) {      
+  for (client in users) {
     if (users[client].email !== email) {
-      console.log("bad email");
-      res.redirect("/login");
-    } else if (users[client].email === email && !bcrypt.compareSync(password, users[client].password)) {
-      console.log("bad password");
-      res.redirect("/login");
-    } else if (users[client].email === email && bcrypt.compareSync(password, users[client].password)) {
-      console.log("logging in");
+      status = 1;
+    } else if (!bcrypt.compareSync(password, users[client].password)) {
+      status = 2;
+    } else if (bcrypt.compareSync(password, users[client].password)) {
+      status = 0;
+      userId = client;
       break;
     }
   }
-  userId = users[client].id;
-  req.session.email = email;
-  req.session.userId = userId;
-  console.log(req.session);
-  res.redirect("/urls");
+
+  if (status === 1) {
+    templateVars.error = "incorrect email";
+    res.redirect("/login", templateVars);
+  } else if (status === 2) {
+    templateVars.error = "incorrect password";
+    res.redirect("/login", templateVars);
+  } else if (status === 0) {
+    req.session.email = email;
+    req.session.userId = userId;
+    res.redirect("/urls");
+  }
 });
 
 //LOGOUT BUTTON PRESS
@@ -236,18 +264,6 @@ app.post("/logout", (req, res) => {
   req.session = null;
   res.redirect("/urls");
 });
-
-//GENERATE SHORT URL STRING
-function generateRandomString() {
-  const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-  let length = characters.length;
-  let result = "";
-  for (let i = 0; i < 5; i++){
-    result += characters.charAt(Math.floor(Math.random() * length));
-  }
-
-  return result;
-};
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
